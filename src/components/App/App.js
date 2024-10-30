506f78f07ae8b8104264b8179b0797a71beb81d6
J.D. KaPow <jim.powers@alun.mit.edu>
2024-10-30 11:30:42 -0400

    final working version

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -188,11 +188,11 @@ function App() {
     await SpotifyManager.redirectToSpotifyAuthorize();
     doSearch('');
     getPlaylists(1);
-  }
+  };
 
   async function handleLogoutClick() {
     await SpotifyManager.logout();
-  }
+  };
 
   return (
     <div className={styles.app}>
@@ -225,7 +225,8 @@ function App() {
                             onAddTrack={onAddTrackHandler}/>
           </div>
         </div>
-        <div className={styles["section-container"]}>  
+        <div className={styles["section-container"]}
+              style={userData===null ? {display:"none"} : {}}>  
           <h3 className={styles.h3}>Manage Your Playlists</h3>   
           <div className={styles["playlist-container"]}>
             <PlaylistManager existingPlaylistItems={existingPlaylistItems} 
